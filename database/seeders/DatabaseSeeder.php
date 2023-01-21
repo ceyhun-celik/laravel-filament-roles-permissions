9e3a449b23ebb1f5dfbfd263cfa10c0e4d28a29c
ceyhun-celik <ceyhuniverse@gmail.com>
2023-01-21 18:07:50 +0300

    setup laravel, filament and spatie laravel-permission

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        $user = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@example.com',
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $user->assignRole($role);
+
+        // $permission = Permission::create(['name' => 'edit articles']);
+    }
+}
